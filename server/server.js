6d8edd03f7df84e5160173010d6997958c0433dc
Carl Ivan Ken Simbulan <carlivan252003@gmail.com>
2026-05-17 07:52:57 +0800

    error

--- server/server.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-// Add this right below your middleware
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected successfully!'))
-    .catch(err => console.error('MongoDB connection error:', err));
